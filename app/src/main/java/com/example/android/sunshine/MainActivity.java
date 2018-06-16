6e74268e6152d8b46bd7c625f1868186f2764162
Iñaki <inaki.diaz@ikasle.egibide.org>
2018-06-16 20:19:41 +0200

    Exercise 02_01

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -15,16 +15,25 @@
  */
 package com.example.android.sunshine;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import com.example.android.sunshine.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mWeatherTextView;
-
+    final String TAG ="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
@@ -35,27 +44,6 @@ public class MainActivity extends AppCompatActivity {
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
         // TODO (4) Delete the dummy weather data. You will be getting REAL data from the Internet in this lesson.
-        /*
-         * This String array contains dummy weather data. Later in the course, we're going to get
-         * real weather data. For now, we want to get something on the screen as quickly as
-         * possible, so we'll display this dummy data.
-         */
-        String[] dummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-                "Friday - Thunderstorms - 21°C / 9°C",
-                "Saturday - Thunderstorms - 16°C / 7°C",
-                "Sunday - Rainy - 16°C / 8°C",
-                "Monday - Partly Cloudy - 15°C / 10°C",
-                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                "Sat, May 28 - Meteors - 16°C / 7°C",
-                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
 
         // TODO (3) Delete the for loop that populates the TextView with dummy data
         /*
@@ -63,16 +51,57 @@ public class MainActivity extends AppCompatActivity {
          * the "\n\n\n" after the String is to give visual separation between each String in the
          * TextView. Later, we'll learn about a better way to display lists of data.
          */
-        for (String dummyWeatherDay : dummyWeatherData) {
-            mWeatherTextView.append(dummyWeatherDay + "\n\n\n");
-        }
 
         // TODO (9) Call loadWeatherData to perform the network request to get the weather
+        loadWeatherData();
     }
 
     // TODO (8) Create a method that will get the user's preferred location and execute your new AsyncTask and call it loadWeatherData
-
+    public void loadWeatherData(){
+        String location = "vitoria";
+        URL weatherSearchUrl = NetworkUtils.buildUrl(location);
+        Log.d(TAG, weatherSearchUrl.toString());
+        new WeatherQueryTask().execute(weatherSearchUrl);
+    }
     // TODO (5) Create a class that extends AsyncTask to perform network requests
+    public class WeatherQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String weatherSearchResults = null;
+            try {
+                weatherSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Log.d(TAG, weatherSearchResults);
+            return weatherSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String weatherSearchResults) {
+            if (weatherSearchResults != null && !weatherSearchResults.equals("")) {
+                mWeatherTextView.setText(weatherSearchResults);
+            }else{
+                Log.e(TAG, "no ha habido respuesta");
+            }
+        }
+    }
+
+
+
+
     // TODO (6) Override the doInBackground method to perform your network requests
     // TODO (7) Override the onPostExecute method to display the results of the network request
+
+
+
+
+
 }
